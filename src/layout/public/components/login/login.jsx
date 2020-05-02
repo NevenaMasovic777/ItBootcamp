7d2504e8382a3465799baa5e35bb56e21846d8de
NevenaMasovic777 <56930985+NevenaMasovic777@users.noreply.github.com>
2020-05-02 14:44:32 +0200

    update

--- src/layout/public/components/login/login.jsx
+++ src/layout/public/components/login/login.jsx
@@ -3,16 +3,12 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./login.css"
 
-<<<<<<< HEAD
-export const Login = () => {
-=======
 export const Login = ({history}) => {
 
   function handleSubmit(){
     history.push('/home')
   }
 
->>>>>>> 54439e321f22962edb81f134aa6daf5f56503dad
   return (
     <>
       <div className="form">
@@ -25,15 +21,11 @@ export const Login = ({history}) => {
             <Form.Group controlId="formBasicPassword">
               <Form.Control type="password" placeholder="Enter Password" />
             </Form.Group>
-
-<<<<<<< HEAD
-            <Button variant="primary" type="submit">
-=======
+            
             <Button variant="primary" type="submit" onClick={e => {
               e.preventDefault()
               handleSubmit()
             }}>
->>>>>>> 54439e321f22962edb81f134aa6daf5f56503dad
               Submit
             </Button>
           </Form>
