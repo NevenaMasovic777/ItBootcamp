11704b7646455b7768bdd2ed7124b2bf033e0420
Nevena Mašović <nevena.masovic@svea.com>
2020-05-02 16:00:30 +0200

    changes update

--- src/layout/private/privateRoute.js
+++ src/layout/private/privateRoute.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Route, Redirect} from 'react-router-dom';
-import { isLogin } from '../../services/auth.service';
+import { Route } from 'react-router-dom';
 
 export const PrivateRoute = ({component: Component, ...rest}) => {
     return (
@@ -8,9 +7,7 @@ export const PrivateRoute = ({component: Component, ...rest}) => {
         // Show the component only when the user is logged in
         // Otherwise, redirect the user to /login page
         <Route {...rest} render={props => (
-            isLogin() ?
             <Component {...props} />
-        : <Redirect to="/login" />
         )} />
 
     );
