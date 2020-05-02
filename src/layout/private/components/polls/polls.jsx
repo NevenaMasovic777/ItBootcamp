1f163b1eec7da6e34c82381165da32e2236c0b1d
NevenaMasovic777 <56930985+NevenaMasovic777@users.noreply.github.com>
2020-05-02 14:47:02 +0200

    updated

--- src/layout/private/components/polls/polls.jsx
+++ src/layout/private/components/polls/polls.jsx
@@ -1,28 +1,15 @@
 import React from "react";
-<<<<<<< HEAD
-import { NewPoll } from "./new-poll";
-import { PollResults } from "./poll-results";
-import "./polls.css"
-=======
 import "./polls.css"
 import { Link } from "react-router-dom";
->>>>>>> 54439e321f22962edb81f134aa6daf5f56503dad
-
 
 
 export const Polls = () => {
   return (
     <div>
-<<<<<<< HEAD
-        <NewPoll></NewPoll>
-        <PollResults></PollResults>
-=======
       <ul>
         <li><Link to="newpoll">New  Poll</Link></li>
         <li><Link to="results">Poll results</Link></li>
       </ul>
-
->>>>>>> 54439e321f22962edb81f134aa6daf5f56503dad
     </div>
   );
 };
