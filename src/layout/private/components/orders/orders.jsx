4abff2b6717803e8e12c8f94119b912975040424
Nevena <nevenasd2@gmail.com>
2020-05-04 10:23:03 +0200

    added header

--- src/layout/private/components/orders/orders.jsx
+++ src/layout/private/components/orders/orders.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { NewOrder } from "./new-order";
 import "./orders.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import { Header } from "../../../public/components/header";
 
 export const Orders = () => {
   return (
     <div>
+      <Header></Header>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="/home">Home</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/orders">Orders</Nav.Link>
+          <Nav.Link href="/settings">Settings</Nav.Link>
+        </Nav>
+        <Link to="login">
+          <button className="btn btn-danger">LOGOUT</button>
+        </Link>
+      </Navbar>
       <div style={{ textAlign: "center" }}>
         <input type="text" value="radna subota" style={{ marginTop: "40px" }} />
       </div>
--- src/layout/private/components/settings/settings.jsx
+++ src/layout/private/components/settings/settings.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./settings.css";
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Header } from "../../../public/components/header";
 
 export const Settings = () => {
   return (
     <div>
-      <div style={{ textAlign: "center" }}>
+        <Header></Header>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="/home">Home</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/orders">Orders</Nav.Link>
+          <Nav.Link href="/settings">Settings</Nav.Link>
+        </Nav>
+        <Link to="login">
+          <button className="btn btn-danger">LOGOUT</button>
+        </Link>
+      </Navbar>
+      {/* <div style={{ textAlign: "center" }}>
         <Link to="home">
           <button
             className="btn btn-primary"
@@ -14,7 +28,7 @@ export const Settings = () => {
             BACK TO MAIN
           </button>
         </Link>
-      </div>
+      </div> */}
       <div className="settings">
         <h4>Settings</h4>
         <div>
